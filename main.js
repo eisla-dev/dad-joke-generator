7d922994c7339ee3e51f460d8d12ea1db2bed381
ajile <22140810+eisla-dev@users.noreply.github.com>
2021-05-27 13:37:22 -0600

    stored the message components

---
+++ main.js
@@ -0,0 +1,3 @@
+let dadJokes = ["Today, my son asked \"Can I have a book mark?\" and I burst into tears. 11 years old and he still doesn't know my name is Brian.", "My wife just completed a 40 week body building program this morning. It's a girl and weighs 7lbs 12 oz", "As I handed my Dad his 50th birthday card, he looked at me with tears in his eyes and said, \"You know, one would have been enough\".", "What genre are national anthems? Country.", "If pronouncing my b's as v's makes me sound Russian, then soviet.", " I am Buzz Aldrin. Second man to step on the moon. Neil before me."]
+ 
+
